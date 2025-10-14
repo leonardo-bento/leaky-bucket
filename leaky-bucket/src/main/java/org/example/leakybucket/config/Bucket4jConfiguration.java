d7e33c6f1251c9e4476a1faa87946c915b5a8b9d
leonardo.bento <leonardo.bento@quintoandar.com.br>
2025-10-14 16:35:05 -0300

    A full functional implementation using Bucket4j

--- leaky-bucket/src/main/java/org/example/leakybucket/config/Bucket4jConfiguration.java
+++ leaky-bucket/src/main/java/org/example/leakybucket/config/Bucket4jConfiguration.java
@@ -1,28 +1,33 @@
 package org.example.leakybucket.config;
 
 import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BandwidthBuilder;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
+import io.github.bucket4j.BucketConfiguration;
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BucketConfiguration {
+public class Bucket4jConfiguration {
+
+    private static final int MAX_BANDWIDTH_CAPACITY_SIMULTANEOUSLY = 1;
 
     @Bean
     public Bucket rateLimitBucket(
-        @Value("${app.bucket.capacity:120}") long capacity,
+        @Value("${app.bucket.capacity:7200}") long capacity,
         @Value("${app.bucket.period:PT1H}") String period
     ) {
         Duration duration = Duration.parse(period);
-        Bandwidth limit = Bandwidth.builder()
-            .capacity(capacity)
-            .refillIntervally(capacity, duration)
+        Bandwidth bandwidth = BandwidthBuilder.builder()
+            .capacity(MAX_BANDWIDTH_CAPACITY_SIMULTANEOUSLY)
+            .refillGreedy(capacity, duration)
+            .initialTokens(0L)
             .build();
         return Bucket.builder()
-            .addLimit(limit)
+            .addLimit(bandwidth)
+            .withMillisecondPrecision()
             .build();
     }
 }
